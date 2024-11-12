d6a5fc5d489b91b3270186166bff9f30d00754a5
Shoaib Qureshi <shoaib@d3vtech.com>
2024-11-12 18:19:03 +0530

    Initial commit

---
+++ resources/views/rows.blade.php
@@ -0,0 +1,36 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Row Number</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Registered at</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $key => $user)
+                            <tr class="{{ ($key + 1) % 2 === 0 ? 'bg-red-100' : '' }}">
+                                <td>{{ $key + 1 }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td class="{{ $key === 0 ? 'font-bold' : '' }}">{{ $user->email }}</td>
+                                <td>{{ $user->created_at }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
