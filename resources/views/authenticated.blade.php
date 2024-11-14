38d7a230cf43c16e8d92e8e14679d05bd58eff0c
Shoaib Qureshi <shoaib@d3vtech.com>
2024-11-14 11:57:25 +0530

    Update authenticated.blade.php

--- resources/views/authenticated.blade.php
+++ resources/views/authenticated.blade.php
@@ -8,11 +8,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    @if (auth()->check())
-                    Yes, I am logged in as {{ auth()->user()->email }}.
-                    @else
-                    No, I am not logged in.
-                    @endif
+                  @auth
+                        Yes, I am logged in as {{ Auth::user()->email }}.
+
+                    @endauth
+                    @guest
+                        No, I am not logged in.
+                    @endguest
                 </div>
             </div>
         </div>
